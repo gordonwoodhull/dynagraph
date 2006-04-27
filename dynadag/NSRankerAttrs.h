61d7d0c43ee84119ba3cb51a0b6d8209c9eb23f9
gordon <gordon@2d18e0bc-46b0-4c4e-aa44-1ad0b8810f46>
2006-04-27 13:06:05 +0000

    forgot files

---
+++ dynadag/NSRankerAttrs.h
@@ -0,0 +1,50 @@
+/**********************************************************
+*      This software is part of the graphviz toolset      *
+*                http://www.graphviz.org/                 *
+*                                                         *
+*            Copyright (c) 1994-2005 AT&T Corp.           *
+*                and is licensed under the                *
+*            Common Public License, Version 1.0           *
+*                      by AT&T Corp.                      *
+*                                                         *
+*        Information and Software Systems Research        *
+*              AT&T Research, Florham Park NJ             *
+*                                                         *
+*               This version available from               *
+*                   http://dynagraph.org                  *
+**********************************************************/
+
+#ifndef NSRankerAttrs_h
+#define NSRankerAttrs_h
+
+#include "Constraints.h"
+
+namespace Dynagraph {
+namespace DynaDAG {
+
+struct NSRankerNode {
+	NodeConstraints topC,bottomC;
+	bool hit; // for rank dfs
+	int newTopRank,	// destination rank assignment
+		newBottomRank,
+		oldTopRank,	// previous rank assignment
+		oldBottomRank;
+	bool rankFixed; // whether nailed in Y
+
+	NSRankerNode() : hit(false),newTopRank(0),newBottomRank(0),oldTopRank(0),oldBottomRank(0),rankFixed(false) {}
+};
+
+struct NSRankerEdge {
+	DDCGraph::Node *weak;
+	DDCGraph::Edge *strong;
+	// the second edge of 2-cycle should be ignored, mostly
+	bool secondOfTwo;
+	
+	NSRankerEdge() : weak(0),strong(0),secondOfTwo(false) {}
+};
+
+
+} // namespace DynaDAG
+} // namespace Dynagraph
+
+#endif // NSRankerAttrs_h
