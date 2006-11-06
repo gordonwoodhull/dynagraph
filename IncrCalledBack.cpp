45c3fac99173df704bb8ef2f6ddacf5bfc749675
gordonwoodhull <gordonwoodhull@2d18e0bc-46b0-4c4e-aa44-1ad0b8810f46>
2006-11-06 03:13:20 +0000

    got rid of old gross configurator

--- IncrCalledBack.cpp
+++ IncrCalledBack.cpp
@@ -47,57 +47,10 @@ IncrCalledBack g_incrPhone;
 
 void IncrCalledBack::incr_cb_create_handler(Name name,StrAttrs &attrs) {
 	attrs = g_defaultGraphAttrs+attrs;
-#ifndef USE_OLD_GROSS_CONFIGURATOR
 	createConfiguration(name,attrs);
 	// hopefully you'll get a more helpful error if something failed but this one is here if the configurator chain itself failed
 	if(!incr_get_handler(name))
 		throw HandlerCreationFailed(); 
-#else
-	DString type = attrs.look("type"),
-		&engines = attrs["engines"],
-		&superengines=attrs["superengines"];
-	if(!type) {
-		if(!engines) {
-			if(superengines.find("shapegen",0)!=DString::npos)
-				engines = "dynadag";
-			else
-				engines = "shapegen,dynadag";
-		}
-		DString::size_type ddpos = engines.find("dynadag",0);
-		if(ddpos!=DString::npos) {
-			if(engines.find("nsranker",0)==DString::npos && superengines.find("nsranker",0)==DString::npos)
-				if(superengines) {
-					string s = superengines.c_str(); // ick
-					s += ",nsranker";
-					superengines = s.c_str();
-				}
-				else {
-					string s = engines.c_str();
-					s.insert(ddpos,"nsranker,");
-					engines = s.c_str();
-				}
-			// BLEEEEE3EEEEEEEEEEE-AAAAAAARRRRRRGFGGGGGYGYGYYYYHHHHHHHHH   IIII HATE DEADLINES1!!!!!!!!1
-			if(engines.find("clearextraranks",0)==DString::npos) {
-				string s = engines.c_str();
-				s.insert(engines.find("dynadag",0),"clearextraranks,");
-				engines = s.c_str();
-			}
-			if(engines.find("edgesuppressor",0)==DString::npos) {
-				string s = engines.c_str();
-				s.insert(engines.find("dynadag",0),"edgesuppressor,");
-				engines = s.c_str();
-			}
-		}		
-		if(engines.find("dynadag",0)!=DString::npos)
-			type = "dynadag";
-		else if(engines.find("fdp",0)!=DString::npos)
-			type = "fdp";
-		else
-			throw DGException2("could not deduce graph type from engine list",engines);
-	}
-    IncrLangEvents *handler = createHandlers(type,name,attrs);
-	incr_set_handler(name,handler);
-#endif
 }
 void IncrCalledBack::incr_cb_destroy_handler(IncrLangEvents *h) {
     delete h;
